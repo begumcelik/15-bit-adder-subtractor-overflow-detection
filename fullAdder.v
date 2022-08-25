3717e2b93f566d19f5d44769761e29ae0036bf1a
begumcelik <41292368+begumcelik@users.noreply.github.com>
2022-08-25 11:28:29 +0200

    Update fullAdder.v

--- fullAdder.v
+++ fullAdder.v
@@ -1,21 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Engineer: Begum Celik
 // 
 // Create Date:    23:19:58 11/13/2019 
 // Design Name: 
 // Module Name:    fullAdder 
 // Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
 //
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
 module fullAdder(A, B, Cin, S, Carry);
